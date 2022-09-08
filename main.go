442fd70ff514e9c92987176ebcc87ea28299f25b
ThyBui07 <buithinguyenthy@gmail.com>
2022-09-09 00:05:38 +0300

    able to have the whole 8 element for 8 lines array

--- main.go
+++ main.go
@@ -18,35 +18,54 @@ func main() {
 	r := bufio.NewReader(file)
 
 	var line []byte
-	var lineT [][]byte
-	for i := 1; ; i++ {
+	var lines [][]byte
+	for i := 1; i <= 855; i++ {
 		line, err = r.ReadBytes('\n')
-		//fmt.Printf(" type: %T", line)
-		//fmt.Printf("[line:%d] %q\n", i, line)
-		//fmt.Println(line)
-		if i == 12 {
-			lineT = append(lineT, line)
-		}
-
-		if i == 21 {
-			lineT = append(lineT, line)
-		}
-
-		if i == 30 {
-			lineT = append(lineT, line)
-		}
-
-		if i == 39 {
-			lineT = append(lineT, line)
-			break
-		}
-		if err != nil {
-			break
-		}
+		lines = append(lines, line)
 	}
+	var dictionary [][][]byte
+	var char [][]byte
+	count := 0
+	//i <= len(lines)-1
+	// fmt.Println(lines[0:9])
+	// fmt.Println(lines[847:855])
 
-	fmt.Printf("%q", lineT)
+	for i := 0; i <= len(lines)-1; i++ {
+		if len(lines[i]) == 1 {
+			continue
+		} else {
+			lines[i] = lines[i][:len(lines[i])-1]
+			//fmt.Println("this is lines after trim: \n", lines[i])
+			char = append(char, lines[i])
+			count++
+			if count == 8 {
+				// char = append(char, lines[i])
+				dictionary = append(dictionary, char)
+				char = [][]byte{}
+				count = 0
+			}
+		}
+
+	}
+	//fmt.Println(dictionary)
+	//fmt.Printf("%q\n", dictionary)
+	//fmt.Println(char)
+	fmt.Println(dictionary[72])
+	test := "he"
+	fmt.Println(len(dictionary))
+	//var display [][][]byte
+	for _, e := range test {
+		char = dictionary[e-32]
+		fmt.Println(char)
+		// for i := 0; i < 8; i++ {
+		// 	display = append(display, dictionary[e-32])
+		// }
+	}
+	// for _, e := range display {
+	// 	fmt.Printf("%s\n", e)
+	// }
+
 	if err != io.EOF {
-		log.Fatal(err)
+		//log.Fatal(err)
 	}
 }
